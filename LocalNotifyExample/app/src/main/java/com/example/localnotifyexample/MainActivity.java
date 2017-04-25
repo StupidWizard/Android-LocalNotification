517d7732e7d4e6ea1112768128a02cbfb2828f2b
toi-nguyen <toi-nguyen@KYCNT1408-08.local>
2017-04-25 19:55:33 +0900

    base UI

--- LocalNotifyExample/app/src/main/java/com/example/localnotifyexample/MainActivity.java
+++ LocalNotifyExample/app/src/main/java/com/example/localnotifyexample/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.localnotifyexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText hour;
+    EditText min;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hour = (EditText) findViewById(R.id.txt_hour);
+        min = (EditText) findViewById(R.id.txt_min);
+
+        findViewById(R.id.btn_clear).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Clear();
+            }
+        });
+
+
+        findViewById(R.id.btn_schedule).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Schedule();
+            }
+        });
+    }
+
+    void Clear() {
+        LocalNotificationUtils.clearLocalNotification(getApplicationContext());
+    }
+
+    void Schedule() {
+
     }
 }
